545ac63f9f5e16f191b6062e77d6c482d11cc731
Jonas Skjodt <skjo@itu.dk>
2024-10-02 11:29:00 +0200

    Chore: assignment06 set up
    Co-authored-by: Alex-itu <lolk@itu.dk>

--- assignment04/SExpr/obj/Debug/net8.0/sexpr.AssemblyInfo.cs
+++ assignment04/SExpr/obj/Debug/net8.0/sexpr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sexpr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c268b14591f5a3fc3b21493aaae85f6508bc25c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af1e508843896ce710c64cb43c846f111df0eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("sexpr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sexpr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
